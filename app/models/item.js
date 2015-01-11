548fb15e6b7cd917d8af1a661ce59e33f076c8dc
fulltom <vergnaudt@gmail.com>
2015-01-11 22:38:41 +0100

    udpate

--- app/models/item.js
+++ app/models/item.js
@@ -3,7 +3,6 @@ var Schema       = mongoose.Schema;
 
 
 var ItemSchema   = new Schema({
-	pseudo: {type : String, default : '', trim : true},
 	createdAt: { type : Date, default : Date.now },
 	tags: {type: []},
 	imageURI : {type : String},
