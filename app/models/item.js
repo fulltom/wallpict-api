c78353f5541ed0c925318c1a63d85278e66193db
fulltom <vergnaudt@gmail.com>
2015-01-13 14:13:09 +0100

    merge and lcean

--- app/models/item.js
+++ app/models/item.js
@@ -1,32 +1,13 @@
 var mongoose     = require('mongoose');
-<<<<<<< HEAD
 var Schema       = mongoose.Schema;
 
-||||||| merged common ancestors
-var Schema       = mongoose.Schema;
-
-
-=======
-var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
-
->>>>>>> FETCH_HEAD
 var ItemSchema   = new Schema({
 	createdAt: { type : Date, default : Date.now },
 	tags: {type: []},
 	imageURI : {type : String},
 	comment : {type : String},
 	likes : {type : Number},
-<<<<<<< HEAD
-	_created_by : { type: Schema.Types.ObjectId, ref: 'User' },
-||||||| merged common ancestors
-	created_by : { type: Number, ref: 'User' }
-
-=======
-	created_by : { type: ObjectId, ref: 'User' }
-
->>>>>>> FETCH_HEAD
-});
+	_created_by : { type: Schema.Types.ObjectId, ref: 'User' }
 
 mongoose.model('Item', ItemSchema);
 module.exports = mongoose.model('Item');
--- app/models/user.js
+++ app/models/user.js
@@ -4,16 +4,10 @@ var Schema       = mongoose.Schema;
 var UserSchema   = new Schema({
 	username: String,
 	password: String,
-	email: String,
+	email: String
 
 });
-<<<<<<< HEAD
 
 mongoose.model('User', UserSchema);
 module.exports = mongoose.model('User');
-||||||| merged common ancestors
-=======
 
-mongoose.model('User', UserSchema);
-module.exports = mongoose.model('User');
->>>>>>> FETCH_HEAD
